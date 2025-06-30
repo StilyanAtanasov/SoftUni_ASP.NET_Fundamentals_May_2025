44ed76e7318b3f529ac599247acca62eadfa7989
Stilyan Atanasov <stilyan2008@gmail.com>
2025-06-30 15:09:33 +0300

    Added Solution for Lecture: 04. Workshop
    Continued the work over the project CinemaApp started in lecture: 02. Exercise: ASP.NET Core Introduction.
    - Created Layers and implemented Services;
    - Implemented CRUD Operations; Data Validations;

---
+++ 04. Workshop/CinemaWeb/CinemaApp.Services.Core/Interfaces/IMovieService.cs
@@ -0,0 +1,20 @@
+﻿using CinemaApp.Web.ViewModels.Movie;
+
+namespace CinemaApp.Services.Core.Interfaces;
+
+public interface IMovieService
+{
+    Task<IEnumerable<MovieCardViewModel>> GetAllMoviesAsync();
+
+    Task AddMovieAsync(MovieFormViewModel model);
+
+    Task<MovieDetailsViewModel?> GetByIdAsync(string id);
+
+    Task<MovieFormViewModel?> GetForEditByIdAsync(string id);
+
+    Task EditAsync(string id, MovieFormViewModel model);
+
+    Task SoftDeleteAsync(string id);
+
+    Task HardDeleteAsync(string id);
+}
---
+++ 04. Workshop/CinemaWeb/CinemaApp/Controllers/MovieController.cs
@@ -0,0 +1,77 @@
+﻿using CinemaApp.Data;
+using CinemaApp.Services.Core.Interfaces;
+using CinemaApp.Web.ViewModels.Movie;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieService _service;
+
+        public MovieController(IMovieService service) => _service = service;
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<MovieCardViewModel> movies = await _service.GetAllMoviesAsync();
+            return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(MovieFormViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            await _service.AddMovieAsync(model);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string id)
+        {
+            MovieDetailsViewModel? model = await _service.GetByIdAsync(id);
+            if (model == null) return NotFound();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            MovieFormViewModel? model = await _service.GetForEditByIdAsync(id);
+            if (model == null) return NotFound();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, MovieFormViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            await _service.EditAsync(id, model);
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string id)
+        {
+            MovieDetailsViewModel? model = await _service.GetByIdAsync(id);
+            if (model == null) return NotFound();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            await _service.SoftDeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
